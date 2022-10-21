81402e8107fddd3d199d4c7db2e887e4c4fa4439
chenyang <chenyang4ad@163.com>
2022-10-21 14:01:33 +0800

    地址

--- app/src/main/java/com/tencent/ncnnyolox/Constant.java
+++ app/src/main/java/com/tencent/ncnnyolox/Constant.java
@@ -2,6 +2,6 @@ package com.tencent.ncnnyolox;
 
 public class Constant {
    public static final String Uploadurl = "camera/posSkuImage";
-   public static final String UploadHosturl = "http://10.128.62.60:8000/";
+   public static final String UploadHosturl = "http://10.0.42.30:8000/";
    public static final String Tag = "99911";
 }
